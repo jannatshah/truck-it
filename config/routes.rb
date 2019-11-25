f03ae4ec5b6b19f599869323860b17e5ab8ce418
Jannat Shah <jannat.shah@gmail.com>
2019-11-25 17:25:48 +0000

    Typo in routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :trucks do
-    resources :schedules, only [:new, :create, :edit, :update]
+    resources :schedules, only: [:new, :create, :edit, :update]
   end
 end
