51d3dccdbbcf3e9824104cbe7108c5c9b91182b3
Jannat Shah <jannat.shah@gmail.com>
2019-11-27 17:02:43 +0000

    add dish page formatting for diets

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -6,6 +6,6 @@ class Dish < ApplicationRecord
   # Enables monetize (money rails)
   monetize :price_cents
   validates :name, presence: true, uniqueness: { scope: :truck }
-  validates :description, length: { minimum: 16, maximum: 140 }
+  validates :description, length: { maximum: 140 }
   validates :price, presence: true, numericality: { other_than: 0 }
 end
