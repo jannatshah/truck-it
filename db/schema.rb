d11a27ab2dfc8619a2b9be989b9893d230fd2308
Jannat Shah <jannat.shah@gmail.com>
2019-11-27 13:40:01 +0000

    Added card to trucks, including the params and the forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_100713) do
+ActiveRecord::Schema.define(version: 2019_11_27_133337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_100713) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "card", default: false
     t.index ["user_id"], name: "index_trucks_on_user_id"
   end
 
