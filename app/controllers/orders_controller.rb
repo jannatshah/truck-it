2f21e58ae716dab38539f0da3e7338cdbcf38af6
daniel makarzec <danielmakarzec@gmail.com>
2019-11-28 15:09:30 +0000

    new selection controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,6 +11,12 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
+    @order.user = current_user
+    if @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
   end
 
   private
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,6 +2,7 @@ class Dish < ApplicationRecord
   belongs_to :truck
   has_many :orders, through: :selections
   has_many :dish_diets
+  has_many :selections
   has_many :diets, through: :dish_diets
   # Enables monetize (money rails)
   monetize :price_cents
