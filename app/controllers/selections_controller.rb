36efbb1dfce0b8e7fdcf123aa223fda6d23a60d5
daniel makarzec <danielmakarzec@gmail.com>
2019-11-29 15:21:08 +0000

    plus and minus buttons

--- app/controllers/selections_controller.rb
+++ app/controllers/selections_controller.rb
@@ -1,8 +1,19 @@
 class SelectionsController < ApplicationController
-  skip_after_action :verify_authorized, only: [:create]
+  skip_after_action :verify_authorized, only: [:create, :destroy]
 
   def create
     @selection = Selection.create(selection_params)
+    @dish = @selection.dish
+  end
+
+  def destroy
+    @selection = Selection.find(params[:id])
+    @dish = @selection.dish
+    @selection.destroy
+    respond_to do |format|
+      format.html { redirect_to truck_path(@dish.truck) }
+      format.js
+    end
   end
 
   private
